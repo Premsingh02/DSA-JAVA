dc0b785325128a9fdf4103d56019088540d2c3f1
Premsingh02 <theaaravkumar02@gmail.com>
2023-07-01 17:04:26 +0530

    started function

---
+++ src/JULY01/function1.java
@@ -0,0 +1,14 @@
+package JULY01;
+
+public class function1 {
+
+    static void function(int x){
+        x = 50;
+        System.out.println(x);
+    }
+    public static void main(String[] args) {
+        int x = 23;
+        function(x);
+        System.out.println(x);
+    }
+}
