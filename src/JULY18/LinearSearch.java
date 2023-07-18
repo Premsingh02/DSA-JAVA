f76bed6da0efcb4b1ef2e6b0220358638d623f80
Premsingh02 <theaaravkumar02@gmail.com>
2023-07-18 22:31:08 +0530

    2 more questions done

--- src/JULY18/LinearSearch.java
+++ src/JULY18/LinearSearch.java
@@ -1,5 +1,21 @@
 package JULY18;
 
 public class LinearSearch {
-    
+    public  static int linearSearch(int numbers[], int key){
+
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    };
+
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10,12,14,16};
+        int key = 10;
+
+//      calling the function
+        System.out.println(linearSearch(numbers,key));
+    }
 }
---
+++ src/JULY18/MenuLinearSearch.java
@@ -0,0 +1,18 @@
+package JULY18;
+
+public class MenuLinearSearch {
+    public static int menuLinearSearch(String menu[],String key){
+        for(int i =0; i<menu.length; i++){
+            if(key == menu[i]){
+                return i;
+            }
+        }
+        return -1;
+    };
+    public static void main(String[] args) {
+        String menu[] = {"Dosa","Burger","Pizza","Mango shake","Chocolate shake"};
+        String key = "Pizza";
+
+        System.out.println(menuLinearSearch(menu,key));
+    }
+}
