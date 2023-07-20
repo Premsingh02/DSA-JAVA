37e0ab283075dec3ff569c2176c200ef20a8f598
Premsingh02 <theaaravkumar02@gmail.com>
2023-07-20 18:43:30 +0530

    1 question done

---
+++ src/JULY20/printSubarrays.java
@@ -0,0 +1,24 @@
+package JULY20;
+
+public class printSubarrays {
+    public static void printSubarrays(int arr[]){
+        int ts = 0;
+        for(int i=0; i<arr.length; i++){
+            int start = i;
+            for(int j=i; j<arr.length; j++){
+                int end = j;
+                for(int k=start; k<=end; k++){
+                    System.out.print(arr[k]+" ");
+                }
+                ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total subarrays = " + ts);
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10};
+        printSubarrays(arr);
+    }
+}
