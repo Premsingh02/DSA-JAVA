52a11c27347ddf2b04b1942c87ca1f23adcdefb9
Premsingh02 <theaaravkumar02@gmail.com>
2023-07-21 11:49:28 +0530

    initial commit of 21 july

---
+++ src/JULY21/Recursion.java
@@ -0,0 +1,19 @@
+package JULY21;
+
+// Function calling itself again and again
+
+public class Recursion {
+    public static void printDec (int n){
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n-1);
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printDec(n);
+    }
+
+}
