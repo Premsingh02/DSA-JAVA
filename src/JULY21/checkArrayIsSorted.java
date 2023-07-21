12e04fc8a6d5eab431d982af1f40f2fe7686fd36
Premsingh02 <theaaravkumar02@gmail.com>
2023-07-21 13:16:12 +0530

    1 more done

---
+++ src/JULY21/checkArrayIsSorted.java
@@ -0,0 +1,17 @@
+package JULY21;
+
+public class checkArrayIsSorted {
+    public static boolean isSorted (int arr[], int i){
+        if(i == arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+        return isSorted(arr,i+1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,5,4};
+        System.out.println(isSorted(arr,0));
+    }
+}
