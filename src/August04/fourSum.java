ff359e8d0b7b5f41d2bb74a85ce3e241f93b6609
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-04 20:02:20 +0530

    initial commit of august 04

---
+++ src/August04/fourSum.java
@@ -0,0 +1,11 @@
+package August04;
+
+public class fourSum {
+    public static int foursum(int[] nums){
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,0,-1,0,-2,2};
+
+    }
+}
