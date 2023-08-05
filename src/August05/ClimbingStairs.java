6d65313afa220b1dc18a6ab3d63e1dba7548fe73
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-05 18:46:40 +0530

    question 3 done

---
+++ src/August05/ClimbingStairs.java
@@ -0,0 +1,20 @@
+package August05;
+
+public class ClimbingStairs {
+    public static int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else {
+            return climbStairs(n - 1) + climbStairs(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println("Number of distinct ways: " + climbStairs(n));
+    }
+}
