8fef946838c852bade96e873f9c7b6134ec99b0c
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-08 21:40:45 +0530

    question 2 done

--- src/August08/SingleNumber.java
+++ src/August08/SingleNumber.java
@@ -1,22 +1,16 @@
-//package August08;
-//
-//public class SingleNumber {
-//    public static int singleNumber(int[] nums){
-//        int result;
-//        for(int i = 0; i<nums.length; i++){
-//            int count = 0;
-//            for(int j=0; j<nums.length; j++){
-//                if(i==j) continue;
-//                if(nums[j]==nums[i]){
-//                    result =  nums[i];
-//                }
-//            }
-//        }
-//        return result;
-//    }
-//    public static void main(String[] args) {
-//        int[] nums = {4,1,2,1,2};
-//        int result = singleNumber(nums);
-//        System.out.println(result);
-//    }
-//}
+package August08;
+
+public class SingleNumber {
+    public static int singleNumber(int[] nums){
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,1,2,1,2};
+        int result = singleNumber(nums);
+        System.out.println(result);
+    }
+}
