f6224719d537daf1b043543defeb36e7db64b92c
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-10 21:26:30 +0530

    question 3 done

---
+++ src/August10/MoveZeros.java
@@ -0,0 +1,16 @@
+package August10;
+
+public class MoveZeros {
+    public int[] moveZeros(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+        return nums;
+    }
+}
