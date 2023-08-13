843b2bb8137a98b523bacfc544aaa860434123ac
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-13 21:23:22 +0530

    question 1 done

---
+++ src/August13/ReverseArraylist.java
@@ -0,0 +1,20 @@
+package August13;
+
+import java.util.ArrayList;
+
+public class ReverseArraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        //reverse printing
+        for(int i=list.size()-1; i>=0; i--){
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
