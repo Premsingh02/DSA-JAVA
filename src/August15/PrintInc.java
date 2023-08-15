2fecd577d337417c85bf28b54949d732d6ab57b9
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-15 19:58:24 +0530

    question 2 done

---
+++ src/August15/PrintInc.java
@@ -0,0 +1,17 @@
+package August15;
+
+public class PrintInc {
+    public static void printInc(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        printInc(n-1);
+        System.out.println(n + " ");
+
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printInc(n);
+    }
+}
