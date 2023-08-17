cef5370cd2789c44d181a12ba1adcb64d8884191
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-17 22:17:45 +0530

    question 3 done

---
+++ src/August17/MedianOfTwoSortedArrays.java
@@ -0,0 +1,51 @@
+package August17;
+
+public class MedianOfTwoSortedArrays {
+    public static double findMedian(int[] nums1, int[] nums2){
+        if (nums1.length > nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+        int totalLength = m + n;
+
+        int left = 0;
+        int right = m;
+
+        while (left <= right) {
+            int partitionX = left + (right - left) / 2;
+            int partitionY = (totalLength + 1) / 2 - partitionX;
+
+            int maxX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int maxY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+
+            int minX = (partitionX == m) ? Integer.MAX_VALUE : nums1[partitionX];
+            int minY = (partitionY == n) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxX <= minY && maxY <= minX) {
+                if (totalLength % 2 == 0) {
+                    return (double) (Math.max(maxX, maxY) + Math.min(minX, minY)) / 2;
+                } else {
+                    return (double) Math.max(maxX, maxY);
+                }
+            } else if (maxX > minY) {
+                right = partitionX - 1;
+            } else {
+                left = partitionX + 1;
+            }
+        }
+
+        // We should never reach this point because the arrays are assumed to be sorted and have at least one median.
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {1,2};
+        int[] nums2 = {3,4};
+        double result = findMedian(nums1, nums2);
+        System.out.println("The Medain is: " + result);
+
+    }
+}
