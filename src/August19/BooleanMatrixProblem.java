07b667e0e872a34dfa58fa719ed6fa435ea9dcf6
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-19 21:02:39 +0530

    question 2 done

---
+++ src/August19/BooleanMatrixProblem.java
@@ -0,0 +1,20 @@
+package August19;
+
+public class BooleanMatrixProblem {
+    public static int[][] fillMatrixRows(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    for (int k = 0; k < n; k++) {
+                        mat[i][k] = 1;
+                    }
+                    break;
+                }
+            }
+        }
+        return mat;
+    }
+    
+}
