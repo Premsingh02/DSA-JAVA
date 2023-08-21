9da50e1f80ebe0bea93a8063c8beb3772487aeaf
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-21 20:55:32 +0530

    Starting hashmap

---
+++ src/August21/hashMap.java
@@ -0,0 +1,18 @@
+package August21;
+
+import java.util.*;
+
+public class hashMap {
+    public static void main(String[] args) {
+        //Creating hashMap
+        HashMap<String,Integer> hm = new HashMap<>();
+
+        //Insert
+        hm.put("India",100);
+        hm.put("China",150);
+        hm.put("US",50);
+
+        //Printing
+        System.out.println(hm);
+    }
+}
