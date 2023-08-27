741e1873f484df4c58ca11d9a01f6a30a8276d9e
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-27 21:44:12 +0530

    question 2 done

---
+++ src/August27/SmallerThantheCurrentNumber.java
@@ -0,0 +1,21 @@
+package August27;
+
+import java.util.*;
+
+public class SmallerThantheCurrentNumber {
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[j] < nums[i]) {
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+
+        return result;
+    }
+}
