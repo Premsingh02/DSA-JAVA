19999765ad752cdff7a78524d6f9b46092c7d47b
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-30 22:47:04 +0530

    question 3 done

---
+++ src/August30/OddorEvenNumbers.java
@@ -0,0 +1,11 @@
+package August30;
+
+public class OddorEvenNumbers {
+    public String checkNumber(int num) {
+        if (num % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+    }
+}
