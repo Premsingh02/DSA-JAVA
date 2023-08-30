6c50717f6a24c69a5a0ea80b99c8540628c2c96c
Premsingh02 <theaaravkumar02@gmail.com>
2023-08-30 22:44:32 +0530

    question 1 done

---
+++ src/August30/SumofSquareNumbers.java
@@ -0,0 +1,12 @@
+package August30;
+
+public class SumofSquareNumbers {
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            double b = Math.sqrt(c - a * a);
+            if (b == (int) b)
+                return true;
+        }
+        return false;
+    }
+}
